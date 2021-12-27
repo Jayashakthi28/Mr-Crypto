77da86e9f744988ee66950360022e169af004490
Jayashakthi28 <vishnu28.js@gmail.com>
2021-12-27 21:31:24 +0530

    removed data.json require

--- src/js/home-page.js
+++ src/js/home-page.js
@@ -1,6 +1,6 @@
 import '../css/index.css'
 import "firebase/app"
 import "./firebase-init"
-require('../assets/data.json');
 import './signin-module';
 import './home-page-coins-update'
+import './top-coin-filler'
