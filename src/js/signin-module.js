317802cd10dd6e621f9cd6da0f69ea89a91a7b60
Jayashakthi28 <vishnu28.js@gmail.com>
2021-12-28 12:16:44 +0530

    modified top coins fill logic

--- src/js/signin-module.js
+++ src/js/signin-module.js
@@ -1,24 +1,34 @@
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { readDB } from "./module/_database";
 import { auth, login, logout } from "./module/_login";
-import {user} from "./module/_user";
+import { user } from "./module/_user";
+import { coinFiller } from "./top-coin-filler";
 
+document.querySelector(".user-icon").addEventListener("click", async () => {
+  console.log(user.getloginStatus());
+  if (!user.getloginStatus()) {
+    const data = await login();
+    console.log(user.getUid(), user.getloginStatus(), user.getCoins());
+  } else {
+    logout();
+  }
+});
 
-document.querySelector('.user-icon').addEventListener("click",async ()=>{
-    console.log(user.getloginStatus());
-    if(!user.getloginStatus()){
-        const data=await login();
-        console.log(user.getUid(),user.getloginStatus(),user.getCoins());        
+onAuthStateChanged(auth, async (firebaseuser) => {
+  if (firebaseuser) {
+    console.log(firebaseuser.uid);
+    await user.setInitial(firebaseuser.uid, firebaseuser.displayName, [], true);
+    let favCoins = await readDB(`${firebaseuser.uid}`);
+    favCoins = favCoins.val();
+    console.log(favCoins);
+    if (favCoins !== null) {
+      favCoins = [...favCoins];
+      console.log(favCoins);
+      await user.setCoins(favCoins);
     }
-    else{
-        logout();
-    }
-})
-
-onAuthStateChanged(auth,(firebaseuser)=>{
-    if(firebaseuser){
-        user.setInitial(firebaseuser.uid,firebaseuser.displayName,[],true);
-    }
-    else{
-        user.removeInitial();
-    }
-})
+    await coinFiller();
+  } else {
+    user.removeInitial();
+    coinFiller();
+  }
+});
