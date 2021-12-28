1392811d7af6a7f49ddb61b13879f94403f29b4e
Jayashakthi28 <vishnu28.js@gmail.com>
2021-12-28 12:15:53 +0530

    modified user method

--- src/js/module/_user.js
+++ src/js/module/_user.js
@@ -1,22 +1,53 @@
-function User(uid='',name='',coins=[],isLogin=false){
-    const getUid=()=> uid;
-    const getCoins=()=>coins;
-    const getloginStatus=()=> isLogin;
-    const setUid=(userid)=>uid=userid;
-    const setCoins=(coinArr)=>coins.push(coinArr);
-    const setloginStatus=(status)=> isLogin=status;
-    const setInitial=()=>{
-        uid=arguments[0];
-        name=arguments[1];
-        coins=arguments[2];
-        isLogin=true;
+import { writeDB } from "./_database";
+
+function User(uid = "", name = "", coins = [], isLogin = false) {
+  const getUid = () => uid;
+  const getCoins = () => coins;
+  const getloginStatus = () => isLogin;
+  const setUid = (userid) => (uid = userid);
+  const setCoins = (coinArr) => (coins = coinArr);
+  const setloginStatus = (status) => (isLogin = status);
+  const setInitial = (user_id, user_name, coin) => {
+    uid = user_id;
+    name = user_name;
+    if (coin !== undefined) {
+      coins = coin;
+    } else {
+      coins = [];
     }
-    const removeInitial=()=>{
-        uid='';
-        coins=[];
-        isLogin=false;
-    }
-    return{getUid,getCoins,getloginStatus,setUid,setCoins,setloginStatus,setInitial,removeInitial}
+    isLogin = true;
+  };
+  const removeInitial = () => {
+    uid = "";
+    coins = [];
+    isLogin = false;
+  };
+  function pushCoin(coin) {
+    console.log(coins, coin);
+    coins.push(coin);
+    writeDB(`${uid}`, coins);
+  }
+  function removeCoin(coin) {
+    let idx = coins.findIndex((d) => {
+      return d === coin;
+    });
+    console.log(idx);
+    coins.splice(idx, 1);
+    writeDB(`${uid}`, coins);
+  }
+
+  return {
+    getUid,
+    getCoins,
+    getloginStatus,
+    setUid,
+    setCoins,
+    setloginStatus,
+    setInitial,
+    removeInitial,
+    pushCoin,
+    removeCoin,
+  };
 }
 
-export const user=User();
+export const user = User();
