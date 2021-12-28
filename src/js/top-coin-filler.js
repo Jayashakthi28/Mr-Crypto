f4db0aea0cb88eed13bac09136fe9db7392dd1a9
Jayashakthi28 <vishnu28.js@gmail.com>
2021-12-28 12:18:18 +0530

    added fav coins select logic

--- src/js/top-coin-filler.js
+++ src/js/top-coin-filler.js
@@ -1,47 +1,77 @@
 import { coins_map } from "./coins_map";
-import { topCoinsDetails } from "./home-page-coins-update";
+import { apiData } from "./module/_apiFetch";
+import { user } from "./module/_user";
 
-(async function coinFiller(){
-    let jsonData=await topCoinsDetails();
-    jsonData=await jsonData['data'];
-    let template='';
-    await jsonData.forEach(data => { 
-        let percent=+data["metrics"]["market_data"]['percent_change_usd_last_24_hours'];
-        let price=+data["metrics"]["market_data"]["price_usd"];
-        percent=percent.toFixed(2);
-        price=price.toFixed(2);
-        console.log(percent,price);
-        template+=`
-        <div class="top-coin">
+export async function coinFiller() {
+  let jsonData = await apiData.getTopcoinData();
+  document.querySelector(".top-coins-cont").innerHTML = "";
+  jsonData = await jsonData["data"];
+  let template = "";
+  await jsonData.forEach((data) => {
+    let percent =
+      +data["metrics"]["market_data"]["percent_change_usd_last_24_hours"];
+    let price = +data["metrics"]["market_data"]["price_usd"];
+    percent = percent.toFixed(2);
+    price = price.toFixed(2);
+    let topCoin = document.createElement("div");
+    topCoin.classList.add("top-coin");
+    topCoin.innerHTML += `
             <div class="coin-rank">
-                ${data['metrics']["marketcap"]["rank"]}
+                ${data["metrics"]["marketcap"]["rank"]}
             </div>
             <div class="coin-logo-cont">
-                <img src="https://s2.coinmarketcap.com/static/img/coins/64x64/${coins_map[data["symbol"]]}.png" alt="">
+                <img src="https://s2.coinmarketcap.com/static/img/coins/64x64/${
+                  coins_map[data["symbol"]]
+                }.png" alt="">
             </div>
             <div class="coin-name">
-                ${data['slug'].charAt(0).toUpperCase()+data['slug'].slice(1)}
+                ${data["slug"].charAt(0).toUpperCase() + data["slug"].slice(1)}
             </div>
             ${
-                (percent<0)?`<div class="coin-price red-txt">
+              percent < 0
+                ? `<div class="coin-price red-txt">
                                 ${price}    
                             </div>
                             <div class="coin-price-change red-txt">
                                 ${percent}
                             </div>`
-                            :
-                            `<div class="coin-price green-txt">
+                : `<div class="coin-price green-txt">
                                 ${price}
                             </div>
                             <div class="coin-price-change green-txt">
                                 ${percent}
                             </div>`
             }
-            <div class="fav-linker">
-                <img src="../assets/star.svg" alt="">
-            </div>
-        </div>
-        `
-    });
-    document.querySelector(".top-coins-cont").innerHTML=await template;
-})();
+            ${
+              user.getCoins().includes(data["symbol"])
+                ? `<div class="fav-linker">
+                    <img class="star" data-coin="${data["symbol"]}" src="../assets/star.svg" alt="">
+                </div>`
+                : `<div class="fav-linker">
+                    <img class="unstar" data-coin="${data["symbol"]}" src="../assets/unstar.svg" alt="">
+                </div>`
+            }
+        `;
+    document.querySelector(".top-coins-cont").appendChild(topCoin);
+    topCoin
+      .querySelector(".fav-linker img")
+      .addEventListener("click", favClicker);
+  });
+  document.querySelector(".top-coins-cont").classList.remove("none");
+}
+
+function favClicker(e) {
+  if (!user.getloginStatus()) return;
+
+  if (e.target.classList.contains("unstar")) {
+    e.target.classList.remove("unstar");
+    e.target.classList.add("star");
+    e.target.src = "../assets/star.svg";
+    user.pushCoin(e.target.dataset.coin);
+  } else {
+    e.target.classList.add("unstar");
+    e.target.classList.remove("star");
+    e.target.src = "../assets/unstar.svg";
+    user.removeCoin(e.target.dataset.coin);
+  }
+}
