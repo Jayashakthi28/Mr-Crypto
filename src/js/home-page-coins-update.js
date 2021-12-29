1616bbe3b9298e14e15ad1cc707756808ed4b1c2
Jayashakthi28 <vishnu28.js@gmail.com>
2021-12-29 16:40:11 +0530

    added tab switch logic

--- src/js/home-page-coins-update.js
+++ src/js/home-page-coins-update.js
@@ -1,3 +1,6 @@
+import { newsFiller } from "./news";
+import { coinFiller } from "./top-coin-filler";
+
 async function majorCardpriceUpdate(){
     const coin_arr=["btc","eth","dot"];
     const elements=document.querySelectorAll(".major-price");
@@ -14,6 +17,28 @@ async function majorCardpriceUpdate(){
 }
 
 
+export function tabSwitcher(){
+    const tab_btn= document.querySelectorAll(".tab_btn");
+   tab_btn.forEach((data,index)=>{
+        data.addEventListener("click",()=>{
+            if(index===0){
+                tab_btn[index].classList.add("focus-card");
+                tab_btn[1].classList.remove("focus-card");
+                tab_btn[2].classList.remove("focus-card");
+                coinFiller();
+            }
+            if(index===1){
+                tab_btn[index].classList.add("focus-card");
+                tab_btn[0].classList.remove("focus-card");
+                tab_btn[2].classList.remove("focus-card");
+                document.querySelector(".top-coins-cont").classList.add("none");
+                document.querySelector(".news-cont").classList.remove("none");
+            }
 
+        });
+    })
+}
+
+tabSwitcher();
 majorCardpriceUpdate();
 export{majorCardpriceUpdate}
